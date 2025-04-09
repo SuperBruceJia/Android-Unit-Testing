39fca1eda0ba512d0c561e9be0f86da9b4eef3ad
Shuyue Jia (Bruce) <shuyuej@ieee.org>
2025-04-09 13:46:12 -0400

    Update CalculatorTest.java

--- app/src/test/java/com/example/unittest/CalculatorTest.java
+++ app/src/test/java/com/example/unittest/CalculatorTest.java
@@ -7,7 +7,7 @@ public class CalculatorTest {
     @Test
     public void evaluatesExpression() {
         Calculator calculator = new Calculator();
-        int sum = calculator.evaluate("1+2");
+        int sum = calculator.evaluate("1+2+3");
         assertEquals(6, sum);
     }
 }
